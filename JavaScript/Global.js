1098843fb168b7157b643a61fd076e5276ca4d1e
kali <kali@sudo.sudo>
2025-11-19 05:34:50 -0500

    first commit

---
+++ JavaScript/Global.js
@@ -0,0 +1,27 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+
+ 
+
+
+  
+        window.location.href = "Home.html";
+
+
+
+
+});
+
+document.getElementsByClassName("login-btn").addEventListener("click", function () {
+
+
+
+
+  
+        window.location.href = "Search.html";
+
+
+
+
+});
+
+
